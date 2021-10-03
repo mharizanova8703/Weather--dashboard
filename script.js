e08cdff7b18db90bd56990a300c92a09450bf1ba
mharizanova8703 <mharizanova@yahoo.com>
2021-10-02 23:34:30 -0400

    updating js
    creating  function display Temperature

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ let days = [
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function getForecast(coordinates) {
+    let apiKey ='8d4ddaa41dd589137d8ef5584615807d'
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(showForecast);
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
