a311a0d2d2be14c2ce3295c08e6e3afe05d21a6e
mharizanova8703 <mharizanova@yahoo.com>
2021-10-05 10:13:47 -0400

    updating js, html
    making dinamic function , generate a Api key , we are wrapping the whole code in Jqerry function, fetching the Api , getting a lon. lan.  and making a secong Api call for a uv index

--- script.js
+++ script.js
@@ -1,10 +1,78 @@
-var ApiKey='8d4ddaa41dd589137d8ef5584615807d';
+$(document).ready(function () {
+  var ApiKey = '8d4ddaa41dd589137d8ef5584615807d'
 
+  $('#search-button').on('click', function (event) {
+    event.preventDefault()
+    let userSearch = $('#search-input').val()
 
+    getCurrentWeather(userSearch)
+  })
 
+  function getCurrentWeather(userSearch) {
+    console.log('userSearch inside getCurrentWeather function', userSearch)
 
+    let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${ApiKey}&units=imperial`
+    console.log('query URL', queryUrl)
 
+    fetch(queryUrl)
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log('data from api call', data)
 
+        let cityName = $('<div>').addClass('card-title').text(data.name)
+        let temp = $('<div>')
+          .addClass('card-text')
+          .text('Temperature: ' + data.main.temp + 'F')
+        let humidity = $('<div>')
+          .addClass('card-text')
+          .text('Humidity: ' + data.main.humidity + '%')
+        let wind = $('<div>')
+          .addClass('card-text')
+          .text('Wind: ' + data.wind.speed + 'MPH')
 
+        $('#currentWeather').append(cityName, temp, humidity, wind)
 
+        let coords = {
+          lon: data.coord.lon,
+          lat: data.coord.lat,
+        }
+        getUvIndex(coords)
+      })
+  }
 
+  function getUvIndex(coords) {
+    console.log(ApiKey)
+    let queryUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${ApiKey}`
+
+    fetch(queryUrl)
+      .then((response) => response.json())
+      .then(function (data) {
+        console.log('UV INDEX DATA', data)
+
+        let UVIndex = data.current.uvi
+        let UVIndexElement = $('<button>')
+          .addClass('card-text btn btn-sm')
+          .text('UV Index: ' + UVIndex)
+
+        if (UVIndex < 3) {
+          UVIndexElement.addClass('btn-success')
+        } else if (UVIndex < 7) {
+          UVIndexElement.addClass('btn-warning')
+        } else {
+          UVIndexElement.addClass('btn-danger')
+        }
+
+        $('#currentWeather').append(UVIndexElement)
+      })
+  }
+
+  var cityArray = []
+  //creating a function when buuton is click
+
+  function showSavedData() {
+    var cityArray = JSON.parse(localStorage.getItem('citylist'))
+    for (var i = 0; i < cityArray.lenght; i++) {
+      console.log('cityArray', cityArray)
+    }
+  }
+})
